d6fb321ca01f3946c0e94db01fe5c477f3d8fea9
Lewis Jenkins <lewis@kohana.co>
2019-02-25 16:07:42 +0000

    One more formatting change to test Git Ignore

--- DigiCard/Model/Professional.cs
+++ DigiCard/Model/Professional.cs
@@ -5,7 +5,7 @@
 
         public string header = "Professional History";
         public string body = "Cambridge Micro Apps\nSenior Software Engineer\nWorking on RT systems with the stock exchange\n\n" +
-                             "Close Air Solutions\nSoftware Engineer\nWorking in defence with governments around the world";
+                             "Close Air Solutions\nSoftware Engineer\nWorking in defence with governments around the world.\nWorked with Simulation and the DIS Network";
 
         public Professional() { }
     }
